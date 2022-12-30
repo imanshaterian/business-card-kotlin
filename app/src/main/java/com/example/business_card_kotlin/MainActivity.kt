bde45718bb328d4a611fd6ec2222faa0d35b1626
iman <imanshaterian@gmail.com>
2022-12-30 10:21:41 +0100

    finishing the elements on the screen. what is left is to finish the looks of it.

--- app/src/main/java/com/example/business_card_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/business_card_kotlin/MainActivity.kt
@@ -4,19 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.business_card_kotlin.ui.theme.BusinesscardkotlinTheme
-import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +26,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
+
                 ) {
                     Main()
                 }
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Main() {
-    Column(){
+    Column {
         Header()
         Info()
     }
@@ -45,19 +45,61 @@ fun Main() {
 
 @Composable
 fun Header() {
-    Image(painter = painterResource(id = R.drawable.snowflake),
-    contentDescription = null)
+    Image(
+        painter = painterResource(id = R.drawable.snowflake),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .size(150.dp)
+    )
     Text(
         text = "iman shaterian".uppercase(),
         Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center)
-    Text(text = "android developer".uppercase())
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = "android developer".uppercase(),
+        Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
 
 }
 
 @Composable
 fun Info() {
-    Text(text = "info")
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        InfoIcon()
+        InfoText("+0123456789")
+    }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        InfoIcon()
+        InfoText("something@gmail.com")
+    }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        InfoIcon()
+        InfoText("@place for id")
+    }
+}
+
+@Composable
+fun InfoIcon() {
+    Image(
+        painter = painterResource(id = R.drawable.snowflake),
+        contentDescription = null,
+        modifier = Modifier
+            .height(46.dp)
+            .padding(8.dp)
+    )
+}
+
+@Composable
+fun InfoText(info: String) {
+    Text(
+        text = info,
+        modifier = Modifier
+            .padding(8.dp)
+    )
 }
 
 @Preview(showBackground = true)
